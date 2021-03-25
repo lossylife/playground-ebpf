67c8faad13d1af30214c6a3e1c92f733109dc71b
lossylife <lossylife@gmail.com>
2021-03-25 14:56:59 +0800

    loader

--- loader/main.go
+++ loader/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/cilium/ebpf"
 	"github.com/vishvananda/netlink"
 )
@@ -14,18 +15,23 @@ func main(){
 
 	collection, err := ebpf.LoadCollection(*filePath)
 	if err != nil {
+		fmt.Printf("load bpf collection failed, %s", err)
 		return
 	}
 
 	nl, err := netlink.LinkByName(*ifName)
 	if err != nil {
+		fmt.Printf("get link failed, %s", err)
 		return
 	}
 
 	for _, tmpProg := range collection.Programs {
 		err = netlink.LinkSetXdpFd(nl, tmpProg.FD())
 		if err != nil {
+			fmt.Printf("link bpf program to network interface failed, %s", err)
 			return
 		}
 	}
+
+	fmt.Printf("load bpf program success\n")
 }
